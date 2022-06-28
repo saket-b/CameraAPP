594554d8848ef80379256d9ecdab7ad119df3f71
saket-b <56158611+saket-b@users.noreply.github.com>
2022-06-28 18:43:22 +0530

    udate gallery.html
    Signed-off-by: saket-b <56158611+saket-b@users.noreply.github.com>

--- gallery.js
+++ gallery.js
@@ -1,158 +1,130 @@
-
 let backBtn = document.querySelector(".back");
-backBtn.addEventListener("click", ()=>{
-    location.assign("./index.html")
+backBtn.addEventListener("click", () => {
+  location.assign("./index.html");
 });
-
-
-setTimeout(()=>{
-
-if( db)
-{
-    let imageDBTRansaction = db.transaction("image", 'readonly');
-
+setTimeout(() => {
+  if (db) {
+    let imageDBTRansaction = db.transaction("image", "readonly");
     let imageStore = imageDBTRansaction.objectStore("image");
-    let imageRquest = imageStore.getAll();
+    let imageRequest = imageStore.getAll();
+    imageRequest.onsuccess = () => {
+      let imageResult = imageRequest.result;
+      let galleryCont = document.querySelector(".gallery-cont");
+      imageResult.forEach((imageObj) => {
+        let imageElem = document.createElement("div");
+        imageElem.setAttribute("class", "media-cont");
+        imageElem.setAttribute("id", imageObj.id);
 
-    imageRquest.onsuccess=()=>{
+        let url = imageObj.url;
+        
 
-        let imageresult = imageRquest.result;
-        let galleryCont = document.querySelector(".gallery-cont");
-
-        imageresult.forEach(( imgobj) => {
-            let imageElem = document.createElement("div");
-            imageElem.setAttribute("class", "media-cont");
-            imageElem.setAttribute("id", imgobj.id);
-
-            let url = imgobj.url;
-
-            imageElem.innerHTML =`
-            <div class= "media>
-            <img src = "${url}"/>
+        imageElem.innerHTML = `
+            <div class="media">
+            <img src="${url}"/>
             </div>
-            <div class = "delete action-btn" > DELETE</div>
-            <div class = "download action-btn">DOWNLOAD</div>
-            `
+            <div class="delete action-btn">DELETE</div>
+            <div class="download action-btn">DOWNLOAD</div>
+        `;
 
-            galleryCont.appendChild(imageElem);
+        galleryCont.appendChild(imageElem);
 
+        let deleteBtn = imageElem.querySelector(".delete");
+        deleteBtn.addEventListener("click", deleteListener);
 
-            let deleteBtn = imageElem.querySelector(".delete");
-            deleteBtn.addEventListener("click", deleteListener);
+        let downloadBtn = imageElem.querySelector(".download");
+        downloadBtn.addEventListener("click", downloadListener);
 
-            let downloadBtn = imageElem.querySelector(".download");
-            downloadBtn.addEventListener("click", downloadListener);
-            
-        });
-    }
+      });
+    };
 
-
-    let videoDBTRansaction = db.transaction("video", 'readonly');
-
+    let videoDBTRansaction = db.transaction("video", "readonly");
     let videoStore = videoDBTRansaction.objectStore("video");
-    let videoRquest = videoStore.getAll();
+    let videoRequest = videoStore.getAll();
+    videoRequest.onsuccess = () => {
+      let videoResult = videoRequest.result;
+      let galleryCont = document.querySelector(".gallery-cont");
 
-    videoRquest.onsuccess=()=>{
+      // for (let i = 0; i < videoResult.length; i++) {
+      //     let videoObj = videoResult[i];
+      // }
 
-        let videoresult = videoRquest.result;
-        let galleryCont = document.querySelector(".gallery-cont");
+      videoResult.forEach((videoObj) => {
+        let videoElem = document.createElement("div");
+        videoElem.setAttribute("class", "media-cont");
+        videoElem.setAttribute("id", videoObj.id);
+        let url = URL.createObjectURL(videoObj.blobData);
 
-        videoresult.forEach(( videoObj) => {
-            let videoElem = document.createElement("div");
-            videoElem.setAttribute("class", "media-cont");
-            videoElem.setAttribute("id", videoObj.id);
-
-            let url = videoObj.url;
-
-            videoElem.innerHTML =`
-            <div class= "media>
-            <img src = "${url}"/>
+        videoElem.innerHTML = `
+            <div class="media">
+              <video autoplay loop src="${url}"></video>
             </div>
-            <div class = "delete action-btn" > DELETE</div>
-            <div class = "download action-btn">DOWNLOAD</div>
-            `
+            <div class="delete action-btn">DELETE</div>
+            <div class="download action-btn">DOWNLOAD</div>
+        `;
 
-            galleryCont.appendChild(videoElem);
+        galleryCont.appendChild(videoElem);
 
+        let deleteBtn = videoElem.querySelector(".delete");
+        deleteBtn.addEventListener("click", deleteListener);
 
-            let deleteBtn = videoElem.querySelector(".delete");
-            deleteBtn.addEventListener("click", deleteListener);
-
-            let downloadBtn = videoElem.querySelector(".download");
-            downloadBtn.addEventListener("click", downloadListener);
-            
-        });
-    }
-}
-
+        let downloadBtn = videoElem.querySelector(".download");
+        downloadBtn.addEventListener("click", downloadListener);
+      });
+    };
+  }
 }, 100);
 
-
-
-function deleteListener(e)
-{
-
-    let id = e.target.parentElement.getAttribute("id");
-    let type = id.split("-")[0];
-    console.log(type);
-
-    if( type == "vid")
-    {
-        let videoDBTRansaction = db.transaction("viedo", "readwrite");
-        let videoStore          = videoDBTRansaction.objectStore("video");
-        videoStore.delete(id);
-    }
-    else if( type == "img")
-    {
-
-        let imgDBTRansaction = db.transaction("image", "readwrite");
-        let imgStore          = imgDBTRansaction.objectStore("image");
-        imgStore.delete(id);
-    }
-
-    e.target.parentElement.remove();
-    
-
+function deleteListener(e) {
+  console.log("hello");
+  let id = e.target.parentElement.getAttribute("id");
+  let type = id.split("-")[0];
+  console.log(type);
+  if (type == "vid") {
+    //remove from database
+    let videoDBTransacrtion = db.transaction("video", "readwrite");
+    let videoStore = videoDBTransacrtion.objectStore("video");
+    videoStore.delete(id);
+  } else if (type == "img") {
+    //remove from database
+    let imageDBTransacrtion = db.transaction("image", "readwrite");
+    let imageStore = imageDBTransacrtion.objectStore("image");
+    imageStore.delete(id);
+  }
+  //remove from ui
+  e.target.parentElement.remove();
 }
 
 function downloadListener(e) {
+  let id = e.target.parentElement.getAttribute("id");
+  let type = id.split("-")[0];
+  if (type == "vid") {
+    let videoDBTransacrtion = db.transaction("video", "readonly");
+    let videoStore = videoDBTransacrtion.objectStore("video");
+    let videoRequest = videoStore.get(id);
+    videoRequest.onsuccess = () => {
+      let videoResult = videoRequest.result;
+      let url = URL.createObjectURL(videoResult.blobData);
 
-    let id = e.target.parentElement.getAttribute("id");
-    let type = id.slit("-")[0];
+      let a = document.createElement("a");
+      a.href = url;
+      a.download = "video.mp4";
+      a.click();
 
-    if( type == "vid")
-    {
-        let videoDBTRansaction = db.transaction("viedo", "readonly");
-        let videoStore          = videoDBTRansaction.objectStore("video");
-        let videoRequest        = videoStore.get(id);
-
-        videoRequest.onsuccess= ()=>{
-            let videoresult = videoRequest.result;
-            let url = URL.createObjectURL(videoresult.blobData);
-
-            let a =document.createElement("a");
-            a.href =url;
-            a.download = "video.mp4";
-            a.click();
-        }
     }
+  }
 
-    if( type == "img")
-    {
-        let imageDBTRansaction = db.transaction("image", "readonly");
-        let imageStore          = imageDBTRansaction.objectStore("image");
-        let imageRequest        = imageStore.get(id);
+  if (type == "img") {
+    let imageDBTransacrtion = db.transaction("image", "readonly");
+    let imageStore = imageDBTransacrtion.objectStore("image");
+    let imageRequest = imageStore.get(id);
+    imageRequest.onsuccess = () => {
+      let imageResult = imageRequest.result;
+      let imageURL = imageResult.url;
 
-        imageRquest.onsuccess = () => {
-
-            let imageResult = imageRquest.result;
-            let imageURL    = imageResult.url;
-            let a = document.createElement("a");
-            a.href = imageURL;
-            a.download = "pic.png";
-            a.click();
-
-        };
-    }
-    
+      let a = document.createElement("a");
+      a.href = imageURL;
+      a.download = "pic.png";
+      a.click();
+    };
+  }
 }
