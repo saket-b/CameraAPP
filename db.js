e3bc60faa3f5c3c5334e35f3bbac6dc9475cfbb6
saket-b <56158611+saket-b@users.noreply.github.com>
2022-06-28 18:38:15 +0530

    updated db.js
    Signed-off-by: saket-b <56158611+saket-b@users.noreply.github.com>

--- db.js
+++ db.js
@@ -1,27 +1,21 @@
-// connect to data base
 
-let db ;
-let openRequest = indexedDB.open('mydatabase');
-
-openRequest.addEventListener('success',()=>{
-
+//connect to database
+let db;
+let openRequest = indexedDB.open('myDatabase');
+let a = 123;
+openRequest.addEventListener('success', () => {
     console.log('db connected');
     db = openRequest.result;
-});
+})
 
-openRequest.addEventListener('upgradeneeded',()=>{
-
-    console.log('db upgraed OR initialised db');
+openRequest.addEventListener("upgradeneeded", () => {
+    console.log("db upgraded OR initalised db ");
     db = openRequest.result;
 
-    db.createObjectStore('video', {keyPath: "id"});
-    db.createObjectStore('image', {keyPath: "id"});
-
-
+    db.createObjectStore('video', { keyPath: "id" });
+    db.createObjectStore("image", { keyPath: "id" });
 });
 
-openRequest.addEventListener('error',()=>{
-
-    console.log('db is error');
-    
+openRequest.addEventListener("error", () => {
+    console.log("db error");
 });
